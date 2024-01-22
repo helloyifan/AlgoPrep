e68012163b991dae8d2fd0850f43f32c2293eae4
Jonathan Dai <yifand@amazon.com>
2024-01-22 15:37:06 -0500

    most wotah

--- Blind75/two_pointers/container-with-most-water.py
+++ Blind75/two_pointers/container-with-most-water.py
@@ -1,17 +1,23 @@
 class Solution():
     def containerWithMostWater(self, nums):
-        # Left to right
-        leftToRightWater = [None] * len(nums)
-        for i, num in enumerate(nums):
-            if i == 0:
-                continue
-            prevI = i - 1
-            prevNum = nums[prevI]
-            leftToRightWater[i] = prevNum - num
-        print(leftToRightWater)
-        return
+        h, t = 0, len(nums) -1
+        maxWater = 0
+        while h < t:
+            # compute water in current rect 
+            lengthOfRect = t - h
+            heightOfRect =  min(nums[h], nums[t])
+            maxWater = max(maxWater, lengthOfRect * heightOfRect)
+
+            # move h in if nums[h] < nums[t]
+            if nums[h] < nums[t]:
+                h += 1
+            # move t in if nums[t] <= nums[h]
+            else:
+                t -= 1
+        return maxWater
 
 
 if __name__ == '__main__':
     s = Solution()
-    s.containerWithMostWater([1, 8, 6, 2, 5, 4, 8, 3, 7])
+    print(s.containerWithMostWater([1, 8, 6, 2, 5, 4, 8, 3, 7]))
+    print(s.containerWithMostWater([1, 1]))
