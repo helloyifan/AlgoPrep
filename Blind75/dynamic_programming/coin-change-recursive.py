b7a3f9f2b879ea12ab27236351b2727bb4710fd5
Jonathan Dai <yfjd01@gmail.com>
2024-01-30 09:03:43 -0500

    triwed asome more

--- Blind75/dynamic_programming/coin-change-recursive.py
+++ Blind75/dynamic_programming/coin-change-recursive.py
@@ -1,3 +1,4 @@
+
 # Spend 20 min and failed wrong way compeletly
 # Spend another 30 mins: i got a solution that works but obvisouly since no DP it takes too long
 class Solution(object):
@@ -7,36 +8,34 @@ class Solution(object):
         :type amount: int
         :rtype: int
         """
-        self.dp = {} # amount: [...]
+        self.dp = {} # amount: length
         self.coins = coins
         self.rets = []
 
-        self.helper([], amount)
+        self.helper(amount)
+        print(self.dp)
 
-        minCoins = float('inf')
-        for ret in self.rets:
-            minCoins = min(minCoins, len(ret))
-
-        if minCoins == float('inf'):
+        if amount not in self.dp:
             return -1
-        return minCoins
+        return self.dp[amount]
 
-    def helper(self, coinsSoFar, amount):
+    def helper(self, amount):
         if amount == 0:
-            sortedCoins = sorted(coinsSoFar)
-            if not sortedCoins in self.rets:
-                self.rets.append(sortedCoins)
-            return 
-
+            return 0 # We are passing back the length starting at length 0
+        
         for coin in self.coins:
             remaining = amount - coin
             if remaining >= 0:
-                coinsSoFarCopy = coinsSoFar[:]
-                coinsSoFarCopy.append(coin)
-                self.helper(coinsSoFarCopy, remaining)
-
-        return 
-
+                ret = self.helper(remaining)
+                ret += 1
+                if amount not in self.dp or self.dp[amount] > ret:
+                    self.dp[amount] = ret
+        return ret 
+    
 if __name__ == '__main__':
     s = Solution()
     print(s.coinChange([1,2,5], 11))
+    print(s.coinChange([2], 3))
+    print(s.coinChange([1], 0))
+    print(s.coinChange([1], 1))
+    print(s.coinChange([1,2147483647], 2))
