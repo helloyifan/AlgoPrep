3545c4797674f3b825818abe731d6460196dedf7
Jonathan Dai <yfjd01@gmail.com>
2024-08-15 09:39:25 -0400

    count-path

---
+++ NeetCode150/2DDynamicProgramming/count-paths.py
@@ -0,0 +1,34 @@
+class Solution:
+    def topDown_uniquePaths(self, m: int, n: int) -> int:
+        if m == 1 and n == 1:
+            return 1
+
+        r1 = 0
+        r2 = 0
+        if m > 1:
+            r1 = self.topDown_uniquePaths(m-1, n)
+        
+        if n > 1:
+            r2 = self.topDown_uniquePaths(m, n-1)
+        
+        return r1 + r2
+    
+    def uniquePaths(self, m, n): #Bottom up, build 0,0 to m,n case
+        dp = [[0 for _ in range(n)] for _ in range(m)] #2d [m][n]
+        for mi in range(0, m):
+            for ni in range(0, n):
+                mIndexVal = 1 #cond if mi == 0 or ni ==0
+                if mi > 0 and ni > 0:
+                    mIndexVal = dp[mi-1][ni] + dp[mi][ni-1]
+                
+                dp[mi][ni] = mIndexVal
+
+        return dp[m-1][n-1]
+
+if __name__ == "__main__":
+    sol = Solution()
+    # print(sol.topDown_uniquePaths(3,3))
+    print(sol.uniquePaths(3,3))
+    print(sol.uniquePaths(3,6))
+
+
