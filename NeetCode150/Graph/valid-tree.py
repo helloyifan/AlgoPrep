2d9d33fadf48dfed2b7dd120403044bb44269a56
Jonathan Dai <yifand@amazon.com>
2024-09-26 09:12:01 -0400

    Valid-tree

--- NeetCode150/Graph/valid-tree.py
+++ NeetCode150/Graph/valid-tree.py
@@ -1,7 +1,7 @@
 # Solved in 10 mins from intuition
-# Kinda looked at the answer 
-# leveraged adjaceny lists, and the fact that in this question, edges are bi directional in idea,
-# but represented as uni-directional (smallNode -> largeNode)
+# Spend another 15 min thinnking more about it and got it 
+# Basically have to make the imrpovement of adding both diferects to adjaceny list
+# and prevent the node from going back to the prev node
 from typing import List
 class Solution:
     def validTree(self, n: int, edges: List[List[int]]) -> bool:
@@ -11,11 +11,13 @@ class Solution:
 
         for e in edges:
             adjList[e[0]].append(e[1])
+            adjList[e[1]].append(e[0])
+            
         print(adjList)
 
         visited = set()
 
-        def dfs(node):
+        def dfs(node, prevNode):
             if node in visited:
                 return False
             
@@ -23,16 +25,25 @@ class Solution:
 
             curList = adjList[node]
             for nextNode in curList:
-                curRet = dfs(nextNode)
+                if nextNode != prevNode:
+                    curRet = dfs(nextNode, node)
 
-                if curRet == False:
-                    return False
+                    if curRet == False:
+                        return False
             
             return True
 
-        ret = dfs(0)
+        ret = dfs(0, -1)
+        if len(visited) != n:
+            ret = False
+
         print(ret)
         return ret
+    
 sol = Solution()
 sol.validTree(5, [[0, 1], [0, 2], [0, 3], [1, 4]]) # True
 sol.validTree(5, [[0, 1], [1, 2], [2, 3], [1, 3], [1, 4]]) # False
+sol.validTree(5, [[0, 1], [2, 1], [3, 2], [3, 1], [4, 1]]) # False
+sol.validTree(4, [[0, 1], [2, 3]]) # False
+sol.validTree(5, [[0, 1], [1, 3], [3, 2], [1, 4]]) # True
+sol.validTree(5, [[0, 1], [2, 0], [3, 0], [1, 4]]) # True
