04d9c99525152a258c772f755fa42289242bd37e
Jonathan Dai <yifand@amazon.com>
2024-11-06 09:02:10 -0500

    count subsequences top down dp

--- NeetCode150/2DDynamicProgramming/count-subsequences.py
+++ NeetCode150/2DDynamicProgramming/count-subsequences.py
@@ -1,57 +1,62 @@
 # This solution doesnt work at all
-# Spent 35min s
-import copy
+# Spent 35min on a solution on tuesday but it was totally wrong
+# Spent 15min got recursion soliution
+# recursiveNumDistinct:
+    # Time Complexity: O(2^(s)) because 
+    # Space Complexity: O(s) for stack (at most we go s levels deep)
+
+# topDownDPnumDistinct:
+    # Time Complexity: O(s*t) 
+    # Space Complexity: O(s*t) for stack and 0(s*t) for dp
+
+# bottomupDP todo:
+
 class Solution:
-    def numDistinct(self, s: str, t: str) -> int:
-
-        breakDownS = []
-
-        prevLetter = None
-        prevLetterIndex = None
-        for i, c in enumerate(s):
-            if prevLetter == None:
-                prevLetter = c
-                prevLetterIndex = i
-            
-            elif prevLetter != c:
-                breakDownS.append(s[prevLetterIndex:i])
-                prevLetter = c
-                prevLetterIndex = i
-        
-        breakDownS.append(s[prevLetterIndex:])
-
-        print(breakDownS)
-
-        # Attempting Recursive solution
-        def dfs(breakDownS, i, remainingT):
-
+    def recursiveNumDistinct(self, s: str, t: str) -> int:
+        def dfs(i, remainingT):
+            if i >= len(s) and remainingT != "":
+                return 0
             if remainingT == "":
                 return 1
-            if i >= len(breakDownS) and remainingT != "":
-                return 0
-            
-            curSCharSet = breakDownS[i]
-            curSChar = curSCharSet[0] # 0 bcuz all characters are teh same
-            
-            ret = 0
-            if curSChar == remainingT[0]:
-                tCopy = copy.deepcopy(remainingT)
-                tCopy = tCopy[1:]
 
-                ret = dfs(breakDownS, i+1,  tCopy)
-                ret *= len(curSCharSet) # cur options
+            ret = 0            
+            if (s[i] == remainingT[0]):
+                ret += dfs(i+1, remainingT[1:])
 
-            ret += dfs(breakDownS, i+1, t)
+            ret += dfs(i+1, remainingT)
             return ret
-        
-        finRet = dfs(breakDownS, 0, t)
+
+        finRet = dfs(0, t)
         print(finRet)
         return finRet
+    
+    def topDownDPnumDistinct(self, s: str, t: str) -> int:
+        dp = {}
 
+        def dfs(i, remainingT):
+            if (i, remainingT) in dp:
+                return dp[(i, remainingT)]
+
+            if i >= len(s) and remainingT != "":
+                return 0
+            if remainingT == "":
+                return 1
+
+            ret = 0            
+            if (s[i] == remainingT[0]):
+                ret += dfs(i+1, remainingT[1:])
+
+            ret += dfs(i+1, remainingT)
+            dp[(i, remainingT)] = ret
+            return ret
+
+        finRet = dfs(0, t)
+        print(finRet)
+        return finRet   
 
 sol = Solution()
-# sol.numDistinct("caaat", "cat") # 3
-# sol.numDistinct("xxyxy", "xy") # 5
+#sol.numDistinct("caaat", "cat") # 3
+sol.numDistinct("xxyxy", "xy") # 5
 
 sol.numDistinct("rabbbit", "rabbit") # 3
 
