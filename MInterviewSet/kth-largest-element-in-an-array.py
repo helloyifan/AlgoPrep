82ef08ac1297b1bde5249afb2105a718e3905580
Jonathan Dai <yfjd01@gmail.com>
2024-12-08 21:36:00 -0500

    kths largest in array with heap

---
+++ MInterviewSet/kth-largest-element-in-an-array.py
@@ -0,0 +1,23 @@
+# Notes: Heapq question
+# TC: for each element, we are performing a O(log k) operation so O(n * logk)
+# SC: At most we are using O(k) space
+import heapq
+from typing import List
+
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        h = []
+
+        for n in nums:
+            if len(h) <= k or n > h[0]:
+                heapq.heappush(h, n) #O(logn)
+            
+            if len(h) > k:
+                heapq.heappop(h) #O(logn)
+        print(h)
+        return h[0]
+    
+sol = Solution()
+sol.findKthLargest([3,2,1,5,6,4], 2)
+sol.findKthLargest([3,2,3,1,2,4,5,5,6], 4)
+sol.findKthLargest([2,1], 2)
