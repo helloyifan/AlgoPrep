a38e5bd34b3a8b3815169443df59ca03d881c133
Jonathan Dai <yfjd01@gmail.com>
2024-12-16 22:21:00 -0500

    LCA 2

--- MInterviewSet/lowest-common-ancestor-of-a-binary-tree-ii.py
+++ MInterviewSet/lowest-common-ancestor-of-a-binary-tree-ii.py
@@ -24,3 +24,52 @@ Return the LCA from the left or right subtree if it exists.
 ## Final Step:
 At the root, if both p and q are found, return the LCA. Otherwise, return null.
 '''
+
+# TC: O(n) to traverse every node of a Binary Tree
+# SC: O(height) if this is a balanced tree because we use call stack (height = log number of nodes), 
+# if this is a skewed tree space is O(n)
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        
+        def dfs(root, p, q):
+            if root == None:
+                return None, None, None #LCA, pFound, qFound
+
+            rLCA, rPFound, rQFound = dfs(root.right, p, q)
+            lLCA, lPFound, lQFound = dfs(root.left, p, q)
+
+            curPFound, curQFound = root == p, root == q
+
+            # If L and R are found
+            if rLCA != None and lLCA != None:
+                return root, True, True
+
+            # If Cur is LCA and either p or q are found
+            if curPFound and (rQFound or lQFound):
+                return root, True, True
+            if curQFound and (rPFound or lPFound):
+                return root, True, True
+            
+            # If we found P or Q return it up
+            if curPFound:
+                return root, True, False
+            if curQFound:
+                return root, False, True
+
+            # Propage results of right exists
+            if rLCA != None:
+                return rLCA, rPFound, rQFound
+            
+            # Propagate result if left exiists
+            if lLCA != None:
+                return lLCA, lPFound, lQFound
+
+            return None, None, None
+
+        lca, pFound, qFound = dfs(root, p, q)
+
+        ret = None
+        if pFound and qFound:
+            ret = lca
+        return ret
