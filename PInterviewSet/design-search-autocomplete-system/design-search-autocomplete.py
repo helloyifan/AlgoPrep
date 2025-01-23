a08501a06b56fb9e35f99592a99c73784401ed46
Jonathan Dai <yifand@amazon.com>
2025-01-23 09:16:00 -0500

    design-search-autocomplete-system/design-search-autocomplete.py

--- PInterviewSet/design-search-autocomplete-system/design-search-autocomplete.py
+++ PInterviewSet/design-search-autocomplete-system/design-search-autocomplete.py
@@ -1,12 +1,23 @@
+# Notes: This solution is different from attempt1 where we maintain a heap for every node
+# This solution uses less space as we dont do that, but has greater TC as we need to do full 
+# DFS to explore every node in the tree once we match the prefix
+# TC:
+# SC:
 from typing import List
 
 class TrieNode:
     def __init__(self, letter):
         self.letter = letter
         self.children = {}
+        self.occurance = None
+        self.sentence = None
 
     def setOccurance(self, occurance):
         self.occurance = occurance
+
+    def setSentence(self, sentence):
+        self.sentence = sentence
+
 
 class Trie:
     def __init__(self):
@@ -22,12 +33,41 @@ class Trie:
             cur = child
         # At the end set the last trie node
         cur.setOccurance(occurance)
+        cur.setSentence(sentence)
+
+    def getSentenceFromPrefix(self, prefix):
+        cur = self.head
+        # Match up to prefix
+        for c in prefix:
+            if not c in cur.children:
+                return []
+            else:
+                cur = cur.children[c]
+
+        #dfs until u find everything
+        ret = []
+        def dfs(cur, ret):
+            if cur == None:
+                return 
+            
+            if cur.occurance != None:
+                ret.append((cur.occurance, cur.sentence))
+            
+            for childKey in cur.children:
+                child = cur.children[childKey]
+                dfs(child, ret)
+
+            return
+        
+        dfs(cur, ret)
+        return ret
 
 
 class AutocompleteSystem:
 
     def __init__(self, sentences: List[str], times: List[int]):
         self.trie = Trie()
+        self.curWord = ''
 
         # Preprocess sentence / times to be on datastruct
         # TC: O(n), SC: O(n)
@@ -41,8 +81,10 @@ class AutocompleteSystem:
             self.trie.addSentence(key, self.data[key])
         return
 
-    curWord = ''
     def input(self, c: str) -> List[str]:
+        ret = []
+        finRet = []
+
         if c == '#':
             if not self.curWord in self.data:
                 self.data[self.curWord] = 1
@@ -52,12 +94,29 @@ class AutocompleteSystem:
             self.trie.addSentence(self.curWord,  self.data[self.curWord])
             # Reset it 
             self.curWord = ''
-
         else:
-            
+            self.curWord += c
+            ret = self.trie.getSentenceFromPrefix(self.curWord)
+            ret = self.helperTopIfSameOccuranceLexigraphicalOrder(ret)
+            ret = ret[:3] # only care about top3 
 
-        return
+            ## remove occurance from final output
+            for r in ret:
+                finRet.append(r[1])
 
+
+        print(finRet)
+        return finRet
+
+    def helperTopIfSameOccuranceLexigraphicalOrder(self, ret):
+        ret.sort(key=lambda x: (-x[0],  x[1])) # the reason why this fking works is wild
+        # this looks like magic and it basically is, 
+        # but -x[0] means we want to first sort this list of tuples by the occurance, however negative maks this a maxheap
+        # so the result is we have a list of (occurance, sentence) first sorted by 
+
+        # x[1] means we want to lexigraphically sort the sentence IN THE CASE WHERE THE OCCURANCES ARE THE SAME
+        # so its kinda just a fluke this sorting works for this specifc question
+        return ret
 
 # Your AutocompleteSystem object will be instantiated and called as such:
 # obj = AutocompleteSystem(sentences, times)
@@ -68,7 +127,11 @@ sol.input('i')
 sol.input(' ')
 sol.input('a')
 sol.input('#')
-# sol.input('i')
-# sol.input(' ')
-# sol.input('a')
-# sol.input('#')
+sol.input('i')
+sol.input(' ')
+sol.input('a')
+sol.input('#')
+sol.input('i')
+sol.input(' ')
+sol.input('a')
+sol.input('#')
