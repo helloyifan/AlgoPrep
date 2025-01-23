faa5d93c74acb5423e6da2a0c0fe02913dc05b6d
Jonathan Dai <yifand@amazon.com>
2025-01-23 09:21:57 -0500

    find-median-from-data-stream/find-median-from-data-stream.py

---
+++ PInterviewSet/find-median-from-data-stream/find-median-from-data-stream.py
@@ -0,0 +1,27 @@
+import heapq
+class MedianFinder:
+
+    def __init__(self):
+        minHeap = []
+        maxHeap = []
+        return
+
+    def addNum(self, num: int) -> None:
+        return
+
+    def findMedian(self) -> float:
+        return        
+
+
+# Your MedianFinder object will be instantiated and called as such:
+# obj = MedianFinder()
+# obj.addNum(num)
+# param_2 = obj.findMedian()
+
+sol = MedianFinder()
+sol.addNum(1)
+sol.addNum(2)
+sol.findMedian()
+sol.addNum(3)
+sol.findMedian()
+
