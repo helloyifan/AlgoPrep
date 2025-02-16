d6c140e558ff76cf7cceb38eb64d6cfed2b1486c
Jonathan Dai <yfjd01@gmail.com>
2025-02-16 12:25:47 -0500

    edited review version

--- PInterviewSet/design-search-autocomplete-system/design-search-autocomplete-review.py
+++ PInterviewSet/design-search-autocomplete-system/design-search-autocomplete-review.py
@@ -3,79 +3,138 @@
 # 1. Store At each level (for better TC, denormalized data worse SC,)
 # 2. Store at root level, (slower/worse, TC better SC)
 
+# Alternative designs
+# Kept track of "time" in a dict instead of a trie
+# Keep track of full sentence at every level instead of just root like mentioned above
+
+
+# TC:
+# Insertion: O(L)
+#   Inserts a word character by character into the Trie. Each word has most L charcters
+
+# Search: O(L + W log W)
+    # Traverses the Trie to find the node corresponding to the prefix (O(L)).
+    # Performs a DFS to collect words (O(W), where W is the total words under the prefix).
+    # Sorts the results (O(W log W)).
+
+# SC
+# Overall Space Complexity: O(N * L) (Trie storage dominates).
+    # If we insert N unique words, the worst case is storing N * L nodes.
+
+# DFS Search Space Complexity: O(W) (temporary space for results).
+
+
+
+
 from typing import List
 
-
 class TrieNode:
-    def __init__(self, letter):
-        self.letter = letter
+    def __init__(self, character):
+        self.character = character
         self.child = {}
-        self.sentence = None
-        self.score = None
+        self.end = False
+        self.times = None
+        self.word = None
+        return
 
 class Trie:
     def __init__(self):
         self.head = TrieNode('')
-        self.dummy = self.head
+
+    def addWord(self, word, times):
+        cur = self.head
+
+        for c in word:
+            if not c in cur.child:
+                cur.child[c] = TrieNode(c)
+
+            cur = cur.child[c]            
+        # Once we reach teh end, cur is the last node
+        cur.end = True
+        cur.times = times
+        cur.word = word
+
+        return
     
-    def add(self, sentence, score):
-        head = self.head
-        for c in sentence:
-            if not c in head.child:
-                head.child[c] = TrieNode(c)
-            head = head.child[c]
+    # Band aid function to get times
+    # Maybe instead we can track times in a another datastructure
+    def getOccuranceOfWord(self, word):
+        cur = self.head
+        for c in word:
+            if not c in cur.child:
+                return 0
+            cur = cur.child[c]
 
-        head.sentence = sentence
-        head.score = score
+        if cur == None:
+            return 0 
+        if cur.times == None:
+            print('wtf')
+            return 0
+        return cur.times
 
-    def get(self, sentence):
-        head = self.head
-        for c in sentence:
-            if not c in head.child:
-                # nothing to return
-                return []
-            else:
-                head = head.child[c]
+    def getWord(self, word):
+        cur = self.head
+
+        for c in word:
+            if not c in cur.child:
+                return [] # maybe should break
+
+            cur = cur.child[c]
 
         # Once we found the point up to sentence, do dfs to get all words
-        ret = []
         def dfs(node):
-            if node.sentence != None:
-                ret.append((node.score, node.sentence))
+            if node == None:
                 return
             
-            for key in node.child:
-                dfs(node.child[key])
-            return
-        
-        dfs(head)
-        return ret
+            ret = []
+            if node.end == True:
+                ret.append((node.times, node.word))
+
+            for key, value in node.child.items():
+                tempRet = dfs(value)
+                ret.extend(tempRet)
+            
+            return ret
+
+        allWords = dfs(cur)
+        self.customSort(allWords)
+        allWords = allWords[:3]
+        return allWords
+    
+    def customSort(self, wordList): #magic sort, first by descending nums, then by lexigraphical
+        # Sort by high priority to low, then sort by acending lexogrphical
+        wordList.sort(key= lambda x: (-x[0], x[1]))
+        return
 
 class AutocompleteSystem:
-
     def __init__(self, sentences: List[str], times: List[int]):
+        self.curInput = ''
+
         self.trie = Trie()
-        self.curInputWord = ''
-
-
-        n = len(sentences)
-        for i in range(n):
-            self.trie.add(sentences[i], times[i])
+        for i in range(len(sentences)):
+            sentence, time = sentences[i], times[i]
+            self.trie.addWord(sentence, time)    
         return
-    
 
     def input(self, c: str) -> List[str]:
-        if c != "#":
-            self.curInputWord += c
-            ret = self.trie.get(self.curInputWord)
-            ret = self.magicTopThreeSort(ret)
+        if c == '#':
+            countOfCurrentSentence = self.trie.getOccuranceOfWord(self.curInput) + 1
 
-            print(ret)
-        return
-    
-    def magicTopThreeSort(self, ret):
-        ret.sort(key=lambda x: (-x[0], x[1]) ) #magic sort, first by descending nums, then by lexigraphical
-        return ret[:3] # top 3 only
+            self.trie.addWord(self.curInput, countOfCurrentSentence ) #shouldnt be one lol
+            self.curInput = '' # reset it
+            return []
+        else:            
+            self.curInput = self.curInput + c
+            ret = self.trie.getWord(self.curInput)
+            # Post processing
+            postPressedRet = []
+            for r in ret:
+                postPressedRet.append(r[1])
+            
+            #print(ret)
+            print(postPressedRet)
+            return postPressedRet
+        
 
 # Your AutocompleteSystem object will be instantiated and called as such:
 # obj = AutocompleteSystem(sentences, times)
@@ -85,12 +144,12 @@ sol = AutocompleteSystem(["i love you", "island", "iroman", "i love leetcode"], 
 sol.input('i')
 sol.input(' ')
 sol.input('a')
-# sol.input('#')
-# sol.input('i')
-# sol.input(' ')
-# sol.input('a')
-# sol.input('#')
-# sol.input('i')
-# sol.input(' ')
-# sol.input('a')
-# sol.input('#')
+sol.input('#')
+sol.input('i')
+sol.input(' ')
+sol.input('a')
+sol.input('#')
+sol.input('i')
+sol.input(' ')
+sol.input('a')
+sol.input('#')
